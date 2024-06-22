e4b6e6a3f466b2cdf92442df4cfbc4e623cdff0c
datahaikuninja <jundai.takashina@gmail.com>
2024-06-22 21:31:01 +0900

    refactor: error handling

--- src/editor.rs
+++ src/editor.rs
@@ -9,21 +9,24 @@ impl Editor {
     }
 
     pub fn run(&self) {
-        enable_raw_mode().unwrap();
+        if let Err(err) = self.repl() {
+            panic!("{err:#?}");
+        }
+        print!("Goodbye.\r\n");
+    }
+    fn repl(&self) -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{:?} \r", event);
-                    if let Char(c) = event.code {
-                        if c == 'q' {
-                            break;
-                        }
+            if let Key(event) = read()? {
+                println!("{:?} \r", event);
+                if let Char(c) = event.code {
+                    if c == 'q' {
+                        break;
                     }
                 }
-                Err(err) => println!("Error: {}", err),
-                _ => (),
             }
         }
-        disable_raw_mode().unwrap();
+        disable_raw_mode()?;
+        Ok(())
     }
 }
