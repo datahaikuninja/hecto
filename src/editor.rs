2ba86a2fded1462d4ca22e383774ef35b8fa8c78
Godai Takashina <azarashi.azarashi.a@gmail.com>
2024-06-23 18:15:54 +0900

    feat: draw tildes

--- src/editor.rs
+++ src/editor.rs
@@ -1,60 +1,62 @@
 use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
 use crossterm::execute;
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use crossterm::style::Print;
+
+mod terminal;
+use terminal::Terminal;
 
 pub struct Editor {
     should_quit: bool,
 }
 
 impl Editor {
-    pub fn default() -> Self {
+    pub const fn default() -> Self {
         Editor { should_quit: false }
     }
     pub fn run(&mut self) {
-        Self::initialize().unwrap();
+        Terminal::initialize().unwrap();
         let result = self.repl();
-        Self::terminate().unwrap();
+        Terminal::terminate().unwrap();
         result.unwrap();
     }
-    fn initialize() -> Result<(), std::io::Error> {
-        enable_raw_mode()?;
-        Self::clear_screen()
-    }
-    fn terminate() -> Result<(), std::io::Error> {
-        disable_raw_mode()
-    }
-    fn clear_screen() -> Result<(), std::io::Error> {
-        let mut stdout = std::io::stdout();
-        execute!(stdout, Clear(ClearType::All))
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let (_, nrow) = Terminal::size()?;
+        for i in 0..nrow {
+            Terminal::move_cursor_to(0, i)?;
+            execute!(std::io::stdout(), Print("~"))?;
+        }
+        Ok(())
     }
     fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
-            let event = read()?;
-            self.evaluate_evnet(&event);
             self.refresh_screen()?;
             if self.should_quit {
                 break;
             }
+            let event = read()?;
+            self.evaluate_evnet(&event);
         }
         Ok(())
     }
     fn evaluate_evnet(&mut self, event: &Event) {
         if let Key(KeyEvent {
-            code, modifiers, ..
+            code: Char(c),
+            modifiers,
+            ..
         }) = event
         {
-            println!("Code: {code:?} Modifiers: {modifiers:?} \r");
-            if let Char(c) = code {
-                if *c == 'q' && *modifiers == KeyModifiers::CONTROL {
-                    self.should_quit = true;
-                }
+            if *c == 'q' && *modifiers == KeyModifiers::CONTROL {
+                self.should_quit = true;
             }
         }
     }
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         if self.should_quit {
-            Self::clear_screen()?;
-            println!("Goodbye!");
+            Terminal::clear_screen()?;
+            print!("Goodbye!\r\n");
+        } else {
+            Self::draw_rows()?;
+            Terminal::move_cursor_to(0, 0)?;
         }
         Ok(())
     }
